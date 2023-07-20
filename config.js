46d342b323262926152e3624a8a2246f1b9987cc
Thatusl <139820020+Thatusl@users.noreply.github.com>
2023-07-21 01:18:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94715598114', 'me', true],
+  ['94715598114', 'desh', true], 
   [''] 
 ] //Number of owners
 
